a99f59d96b779fc4471ccd6b1e64803586f3c894
jetalictic <khuzan557@gmail.com>
2018-12-05 00:05:09 +0800

    adds

--- resources/views/agent.blade.php
+++ resources/views/agent.blade.php
@@ -20,7 +20,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Agent</h1>
+            <h1 class="m-0 text-dark">Agents</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -4,7 +4,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="/dashboard" class="nav-link">Home</a>
+        <a href="/dashboard" class="nav-link"></a>
       </li>
       <!-- <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
